d3c8cd42b6e35846417efda9cdb4ee2ecf6d0e1a
Rahul Pandey <therahulogy@gmail.com>
2023-09-25 10:32:25 +0530

    Add files via upload

---
+++ TechDepartment.java
@@ -0,0 +1,27 @@
+package DepartmentManagement;
+
+class TechDepartment extends SuperDepartment {
+	
+	@Override
+	public String departmentName() {
+		
+		return "Tech Department";
+	}
+
+	@Override
+	public String getTodaysWork() {
+		
+		return "Complete coding of Module 1";
+	}
+
+	@Override
+	public String getWorkDeadline() {
+		
+		return "Complete by EOD";
+	}
+
+	public String getTechStackInformation() {
+		
+		return "Core Java";
+	}
+}
